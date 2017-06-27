a326de1a4666ba2c8e209e585e58c531790287f3
Joe Mizzi <themizzi@me.com>
2017-06-26 20:00:01 -0500

    removed strict_types for php 5.6

--- tests/CommandTest.php
+++ tests/CommandTest.php
@@ -1,7 +1,5 @@
 <?php
 
-declare(strict_types=1);
-
 use App\CoverageCommand;
 use org\bovigo\vfs\vfsStream;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
--- tests/TestKernel.php
+++ tests/TestKernel.php
@@ -1,7 +1,5 @@
 <?php
 
-declare(strict_types=1);
-
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
 
 /**
