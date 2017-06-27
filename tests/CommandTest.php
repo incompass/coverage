eb3ac85842f265d9fe494cdae7ea712e51dac5da
Joe Mizzi <themizzi@me.com>
2017-06-26 19:31:12 -0500

    Added tests

---
+++ tests/CommandTest.php
@@ -0,0 +1,79 @@
+<?php
+
+declare(strict_types=1);
+
+use App\CoverageCommand;
+use org\bovigo\vfs\vfsStream;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+use Symfony\Component\Console\Application;
+use Symfony\Component\Console\Tester\CommandTester;
+
+/**
+ * Class CommandTest
+ *
+ * @author  Joe Mizzi <joe@casechek.com>
+ */
+class CommandTest extends KernelTestCase
+{
+    /**
+     * @var CommandTester
+     */
+    private $tester;
+
+    /**
+     * Setup the filesystem.
+     */
+    protected function setUp()
+    {
+        vfsStream::setup('test');
+        self::bootKernel();
+        $application = new Application(self::$kernel);
+        $application->add(new CoverageCommand());
+        $command = $application->find('coverage');
+        $this->tester = new CommandTester($command);
+    }
+
+    /**
+     * @test
+     */
+    public function it_throws_an_exception_if_file_not_found()
+    {
+        self::expectException(InvalidArgumentException::class);
+        $this->tester->execute(
+            [
+                'file' => vfsStream::url('test/test.xml'),
+                'percentage' => 75,
+            ]
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function it_returns_success_if_coverage_met()
+    {
+        $file = vfsStream::newFile('test/test.xml');
+        file_put_contents($file->url(), '<coverage><project><metrics elements="6" coveredelements="6"/></project></coverage>');
+        self::assertEquals(0, $this->tester->execute(
+            [
+                'file' => $file->url(),
+                'percentage' => 75,
+            ]
+        ));
+    }
+
+    /**
+     * @test
+     */
+    public function it_returns_error_if_coverage_not_met()
+    {
+        $file = vfsStream::newFile('test/test.xml');
+        file_put_contents($file->url(), '<coverage><project><metrics elements="6" coveredelements="0"/></project></coverage>');
+        self::assertNotEquals(0, $this->tester->execute(
+            [
+                'file' => $file->url(),
+                'percentage' => 75,
+            ]
+        ));
+    }
+}
---
+++ tests/TestKernel.php
@@ -0,0 +1,83 @@
+<?php
+
+declare(strict_types=1);
+
+use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
+
+/**
+ * Class TestKernel
+ *
+ * @author  Joe Mizzi <joe@casechek.com>
+ */
+class TestKernel extends \Symfony\Component\HttpKernel\Kernel
+{
+    use MicroKernelTrait;
+
+    /**
+     * Returns an array of bundles to register.
+     *
+     * @return \Symfony\Component\HttpKernel\Bundle\BundleInterface[] An array of bundle instances
+     */
+    public function registerBundles()
+    {
+        return [new \Symfony\Bundle\FrameworkBundle\FrameworkBundle()];
+    }
+
+    /**
+     * Add or import routes into your application.
+     *
+     *     $routes->import('config/routing.yml');
+     *     $routes->add('/admin', 'AppBundle:Admin:dashboard', 'admin_dashboard');
+     *
+     * @param \Symfony\Component\Routing\RouteCollectionBuilder $routes
+     */
+    protected function configureRoutes(\Symfony\Component\Routing\RouteCollectionBuilder $routes)
+    {
+
+    }
+
+    /**
+     * Configures the container.
+     *
+     * You can register extensions:
+     *
+     * $c->loadFromExtension('framework', array(
+     *     'secret' => '%secret%'
+     * ));
+     *
+     * Or services:
+     *
+     * $c->register('halloween', 'FooBundle\HalloweenProvider');
+     *
+     * Or parameters:
+     *
+     * $c->setParameter('halloween', 'lot of fun');
+     *
+     * @param \Symfony\Component\DependencyInjection\ContainerBuilder $c
+     * @param \Symfony\Component\Config\Loader\LoaderInterface $loader
+     */
+    protected function configureContainer(
+        \Symfony\Component\DependencyInjection\ContainerBuilder $c,
+        \Symfony\Component\Config\Loader\LoaderInterface $loader
+    ) {
+        $c->loadFromExtension('framework', [
+            'secret' => 'test'
+        ]);
+    }
+
+    /**
+     * @return string
+     */
+    public function getCacheDir()
+    {
+        return $this->getRootDir() . '/../var/cache';
+    }
+
+    /**
+     * @return string
+     */
+    public function getLogDir()
+    {
+        return $this->getRootDir() . '/../var/logs';
+    }
+}
